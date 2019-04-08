20329c2d5aae98585ecb74fe097a1e530038e118
Stahlwalker <Stahl1109@gmail.com>
2019-04-08 09:44:45 -0500

    completed tutorial added send on keypress

--- public/chat.js
+++ public/chat.js
@@ -4,9 +4,10 @@ var socket = io.connect('http://localhost:4000');
 //Query DOM
 
 var mesage = document.getElementById('message');
-    handle = document.getElementById('handle');
-    btn = document.getElementById('send');
-    output = document.getElementById('output');
+    handle = document.getElementById('handle'),
+    btn = document.getElementById('send'),
+    output = document.getElementById('output'),
+    feedback = document.getElementById('feedback');
 
 
 //Emit events
@@ -18,8 +19,24 @@ btn.addEventListener('click', function(){
     });
 });
 
+message.addEventListener('keypress', function(){
+    socket.emit('typing', handle.value);
+});
+
 //Listen for events
 
 socket.on('chat', function(data){
-    output.innerHTML += "<p><strong>" + data.handle + ':</strong>' + data.message + '</p>';
+    feedback.innerHTML = "";
+    output.innerHTML += "<p><strong>" + data.handle + ': </strong>' + data.message + '</p>';
+});
+
+socket.on('typing', function(data){
+    feedback.innerHTML = '<p><em>' +data + ' is typing a message...</em></p>';
+});
+
+$('input[type="text"]').keypress(function (e) {
+    var code = e.keyCode || e.which;
+    if (code === 13)
+    e.preventDefault();
+    $("send").submit(); /*add this, if you want to submit form by pressing `Enter`*/
 });
