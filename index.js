19916b0ea76d2978513788a8670eb7fa86930311
Stahlwalker <Stahl1109@gmail.com>
2019-04-08 10:47:56 -0500

    test origin

--- index.js
+++ index.js
@@ -25,7 +25,14 @@ io.on('connection', function(socket){
         socket.broadcast.emit('typing', data);
     });
 
-    io.origins(['https://stahlwalker.github.io/websockets/']);
+    // io.origins(['https://stahlwalker.github.io/websockets/']);
+
+    io.origins((origin, callback) => {
+    if (origin !== 'https://stahlwalker.github.io/websockets/') {
+        return callback('origin not allowed', false);
+    }
+    callback(null, true);
+  });
 });
 
 // io.origins((origin, callback) => {
