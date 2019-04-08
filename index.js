4777740af6a905f9a67245e8bb0028699a37a697
Stahlwalker <Stahl1109@gmail.com>
2019-04-08 10:13:32 -0500

    origin update to fix safari

--- index.js
+++ index.js
@@ -25,3 +25,10 @@ io.on('connection', function(socket){
         socket.broadcast.emit('typing', data);
     });
 });
+
+io.origins((origin, callback) => {
+    if (origin !== 'https://stahlwalker.github.io/websockets/') {
+        return callback('origin not allowed', false);
+    }
+    callback(null, true);
+  });
