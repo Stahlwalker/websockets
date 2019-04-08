172bca7747985a7b642fb284979b1e274fad4df2
Stahlwalker <Stahl1109@gmail.com>
2019-04-08 10:54:50 -0500

    updating chat local

--- index.js
+++ index.js
@@ -27,12 +27,12 @@ io.on('connection', function(socket){
 
     // io.origins(['https://stahlwalker.github.io/websockets/']);
 
-    io.origins((origin, callback) => {
-    if (origin !== 'https://stahlwalker.github.io/websockets/') {
-        return callback('origin not allowed', false);
-    }
-    callback(null, true);
-  });
+//     io.origins((origin, callback) => {
+//     if (origin !== 'https://stahlwalker.github.io/websockets/') {
+//         return callback('origin not allowed', false);
+//     }
+//     callback(null, true);
+//   });
 });
 
 
